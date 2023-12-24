4db14d5b96c29f120ff85d0c33ae2c3b4b4980a2
metaeye <li_yuangang@icloud.com>
2023-12-25 01:39:30 +0800

    Remove unused code in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,15 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
-use noter::menu_manager::MenuManager;
-
-// #[tauri::command]
-// async fn initialize_menu() -> String {
-//     let mut menu_manager = MenuManager::new();
-//     // menu_manager.test_init_root();
-//     menu_manager.serialize()
-// }
-
 fn main() {
     tauri::Builder
         ::default()
